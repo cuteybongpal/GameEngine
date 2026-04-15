e7eac990d7c6ef49f0e26438725088058253f72e
hyowonKim <khw33352182@gmail.com>
2026-04-15 21:30:01 +0900

    feat: add stack trace logging for debugging COM object cleanup

--- GameEngine/engine.h
+++ GameEngine/engine.h
@@ -6,11 +6,17 @@
 #include "imgui_impl_win32.h"
 #include <d3d11.h>
 #include <tchar.h>
+#include <windows.h>
+#include <dbghelp.h>
+#include <iostream>
+#include <string>
+
+#pragma comment(lib, "Dbghelp.lib")
 
 #define UNITSIZE 64
 //x가 false라면 강제 중단함
 #define CHECK(x) { if (!x) __debugbreak(); }
-
+#define STACK(x) { PrintStackTrace(x); }
 typedef uint64_t uuid;
 
 typedef enum AssetType
@@ -21,3 +27,45 @@ typedef enum AssetType
 	scene,
 	script
 } assetType;
+
+void PrintStackTrace(unsigned int skipFrames = 1)
+{
+    HANDLE process = GetCurrentProcess();
+
+    static bool initialized = false;
+    if (!initialized)
+    {
+        SymInitialize(process, nullptr, TRUE);
+        initialized = true;
+    }
+
+    void* stack[64];
+    USHORT frameCount = CaptureStackBackTrace(skipFrames, 64, stack, nullptr);
+
+    SYMBOL_INFO* symbol = (SYMBOL_INFO*)calloc(sizeof(SYMBOL_INFO) + 256 * sizeof(char), 1);
+    if (symbol == nullptr)
+        return;
+
+    symbol->SizeOfStruct = sizeof(SYMBOL_INFO);
+    symbol->MaxNameLen = 255;
+
+    DWORD64 displacement = 0;
+
+    for (USHORT i = 0; i < frameCount; i++)
+    {
+        DWORD64 address = (DWORD64)(stack[i]);
+
+        if (SymFromAddr(process, address, &displacement, symbol))
+        {
+            std::cout << i << ": " << symbol->Name
+                << " [0x" << std::hex << symbol->Address << std::dec << "]\n";
+        }
+        else
+        {
+            std::cout << i << ": " << "Unknown"
+                << " [0x" << std::hex << address << std::dec << "]\n";
+        }
+    }
+
+    free(symbol);
+}
